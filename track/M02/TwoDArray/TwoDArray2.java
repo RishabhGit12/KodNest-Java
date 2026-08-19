e3b6871029ce1209c34f35617158361ed16dcebb
Rishabh Patel <rishsbh1478patel@gmail.com>
2026-08-19 13:12:06 +0530

    2D Array 2

--- track/M02/TwoDArray/TwoDArray2.java
+++ track/M02/TwoDArray/TwoDArray2.java
@@ -2,11 +2,16 @@ package track.M02.TwoDArray;
 
 import java.util.Scanner;
 
-public class TwoDArray {
+public class TwoDArray2 {
 
     public static void main(String[] args) {
+        //3 Classes with 4,3 and 5 Students in them
         Scanner sc = new Scanner(System.in);
-        int a[][] = new int[3][5];
+        int a[][] = new int[3][];
+        a[0] = new int[4];
+        a[1] = new int[3];
+        a[2] = new int[5];
+
         System.out.println("Insert Values: ");
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < a[i].length; j++) {
@@ -16,7 +21,7 @@ public class TwoDArray {
 
         System.out.println("Values are: ");
         for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < a[0].length; j++) {
+            for (int j = 0; j < a[i].length; j++) {
                 System.out.print(a[i][j] + " ");
             }
             System.out.println();
